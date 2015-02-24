c200749bca2c84d606afa2341b2a129664de46e4
Aiyappa Ganesh <aiyappa@b-eagles.com>
2015-02-24 16:32:31 +0530

    Home page updates

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
     var height = $(window).height() * 0.95;
     $('.section').height(height);
-    $('.content').height(height * 6);
 });
 
 $(document).ready(function(){
