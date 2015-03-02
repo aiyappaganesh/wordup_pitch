81786c6cf22d5ceff37242cc339cebdbebd4c322
Aiyappa Ganesh <aiyappa@b-eagles.com>
2015-03-02 10:09:38 +0530

    Updates to the homepage

--- handlers/web/index.py
+++ handlers/web/index.py
@@ -8,13 +8,11 @@ class HomePage(WebRequestHandler):
         template_values['section2'] = {}
         template_values['section2']['carousel'] = {}
         template_values['section2']['carousel']['slides'] = []
-        template_values['section2']['carousel']['slides'].append({'img':'/assets/img/screen_1.png','copy':'Share your audio music message'})
-        template_values['section2']['carousel']['slides'].append({'img':'/assets/img/screen_2.png','copy':'Simply type what <br> you want to see'})
-        template_values['section2']['carousel']['slides'].append({'img':'/assets/img/screen_3.png','copy':'Select the song that<br>matches your words best'})
-        template_values['section2']['carousel']['slides'].append({'img':'/assets/img/screen_4.png','copy':'Pick the exact lyrics<br>to convey your message'})
-        template_values['section2']['carousel']['slides'].append({'img':'/assets/img/screen_5.png','copy':'Pick the exact lyrics<br>to convey your message'})
-        template_values['section2']['carousel']['slides'].append({'img':'/assets/img/screen_6.png','copy':'Add photos<br>(optional)'})
-        template_values['section2']['carousel']['slides'].append({'img':'/assets/img/screen_7.png','copy':'Add photos<br>(optional)'})
+        template_values['section2']['carousel']['slides'].append({'img':'/assets/img/screen_1.png','copy':'Simply type what you want to say'})
+        template_values['section2']['carousel']['slides'].append({'img':'/assets/img/screen_1.gif','copy':'Simply type what you want to say'})
+        template_values['section2']['carousel']['slides'].append({'img':'/assets/img/screen_2.png','copy':'Pick the exact lyrics to convey your message'})
+        template_values['section2']['carousel']['slides'].append({'img':'/assets/img/screen_3.png','copy':'Add Photos<br>(optional)'})
+        template_values['section2']['carousel']['slides'].append({'img':'/assets/img/screen_4.png','copy':'Share your audio music message'})
         template_values['section5'] = {}
         template_values['section5']['targets'] = []
         template_values['section5']['targets'].append({'img':'/assets/img/phone_black.png','copy':'1.75B Smartphone users'})
